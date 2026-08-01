ee1ca3ea6bb68614cfc8e0797d90f33fa3f66748
Ömer Sanık <omersanik2016@gmail.com>
2026-08-01 18:40:59 +0200

    Refactor VaR and Expected Shortfall calculations

--- src/returns.py
+++ src/returns.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from data_loader import save_to_csv
 
 def load_data(filepath):
     data = pd.read_csv(filepath)
@@ -8,14 +9,18 @@ def load_data(filepath):
 
 
 def calculate_returns(data):
-    data["Returns"] = (data["Close"] - data["Close"].shift(1)) / data["Close"].shift(1)
+    data["Returns"] = data["Close"].pct_change()
     data = data.dropna()
 
     return data
+
+
 
 if __name__ == "__main__":
 
     data = load_data("data/raw/AAPL.csv")
     data = calculate_returns(data)
+    save_to_csv(data, "data/processed/AAPL_returns.csv")
+
     print(data.head())
     print(data.columns)
---
+++ src/var.py
@@ -0,0 +1,45 @@
+import numpy as np 
+
+
+
+def sort_returns(returns):
+   sorted_returns = returns.sort_values()
+   return sorted_returns
+
+def calculate_cutoff_index(returns, confidence_level):
+    number_of_returns = len(returns)
+
+    cutoff_index = int(number_of_returns * (1 - confidence_level))
+    return cutoff_index
+def calculate_var(returns, confidence_level=0.95):
+    cutoff_index = calculate_cutoff_index(returns, confidence_level)
+    var = returns.iloc[cutoff_index]
+    return var
+
+
+def calculate_expected_shortfall(returns, confidence_level=0.95):
+    cutoff_index = calculate_cutoff_index(returns, confidence_level)
+    tail_losses = returns.iloc[:cutoff_index + 1]
+    expected_shortfall = tail_losses.mean()
+    return expected_shortfall
+
+
+from returns import load_data, calculate_returns
+
+if __name__ == "__main__":
+    data = load_data("data/raw/AAPL.csv")
+
+    data = calculate_returns(data)
+
+    sorted_returns = sort_returns(data["Returns"])
+    my_var = calculate_var(sorted_returns)
+
+    print(sorted_returns.head())
+
+    numpy_var = np.percentile(data["Returns"], 5)
+    print(f"NumPy VaR: {numpy_var:.6f}")
+
+    my_es = calculate_expected_shortfall(sorted_returns)
+
+    print(f"My VaR: {my_var:.6f}")
+    print(f"My ES : {my_es:.6f}")
